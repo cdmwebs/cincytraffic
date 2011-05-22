b551a14c7b5f91b6ccd4fd004e37ffeada043ad4
Chris Moore <cdmwebs@gmail.com>
2011-05-21 23:45:09 -0400

    update to 3.1rc1

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env) if defined?(Bundler)
 
-module ArtimisRails
+module Artimis
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -35,13 +35,20 @@ module ArtimisRails
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    # Please note that JavaScript expansions are *ignored altogether* if the asset
+    # pipeline is enabled (see config.assets.enabled below). Put your defaults in
+    # app/assets/javascripts/application.js in that case.
+    #
     # JavaScript files you want as :defaults (application.js is always included).
-    config.action_view.javascript_expansions[:defaults] = %w()
+    # config.action_view.javascript_expansions[:defaults] = %w(prototype prototype_ujs)
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+
+    # Enable the asset pipeline
+    config.assets.enabled = true
   end
 end
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-ArtimisRails::Application.config.secret_token = 'be46c0498897f9abca5b8d91769d5f727143c199f93d03215a46d3f0391cac86c5fe531388e50cf2e2820925506fccad8e43c8fb3d0576f2069d4a07dbcc75f4'
+Artimis::Application.config.secret_token = '776934a1d9348736c7c086a691900d36f7e00ffde4b3d5bbf21ec3017fb3bf3a1fc8bf22814fec65edd36d866693fb7c49d265a3e7b08594784e0fd7d6d4c26f'
